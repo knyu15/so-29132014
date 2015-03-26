16914572812d4d25bbcc14de2d3f3c4d3271d7a1
knyu15 <knigin.yuri@gmail.com>
2015-03-26 14:59:29 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using wcf_29132014.com.hotschedules.services;
+
+namespace wcf_29132014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new SalesItemServiceService();
+            var date = ToSimpleDate(new DateTime());
+            client.setSalesItemsV3(1, 1, null, date, date);
+        }
+
+        static hsSimpleDate ToSimpleDate(DateTime time)
+        {
+            return new hsSimpleDate
+            {
+                year = time.Year,
+                month = time.Month,
+                day = time.Day,
+            };
+        }
+    }
+}
